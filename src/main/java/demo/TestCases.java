7bec0fa4dccf88d1fb377fa6fc5c6b1e65865bd4
BalajiM <mebaalee@gmail.com>
2024-05-08 21:15:34 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,7 +1,11 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -48,9 +52,62 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.get("https://leetcode.com/");
+        String url = driver.getCurrentUrl();
+        if(url.contains("leetcode")){
+            System.out.println("The Home page contains Leetcode");
+        }
+        else
+            System.out.println("The Home page doesn't contains Leetcode");
+        System.out.println("end Test case: testCase01");
     }
 
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start Test case: testCase02");
+        driver.findElement(By.xpath("//p[text()='View Questions ']")).click();
+        String url = driver.getCurrentUrl();
+        if(url.contains("problemset")){
+            System.out.println("The Home page contains problemset");
+        }
+        else
+            System.out.println("The Home page doesn't contains problemset");
 
+        List<WebElement> titles = driver.findElements(By.xpath("//a[@class = 'h-5 hover:text-blue-s dark:hover:text-dark-blue-s']"));
+        Thread.sleep(5000);
+        for(int i = 1; i <= 5; i++){
+            String titleName = titles.get(i).getText();
+            System.out.println(titleName);
+
+            }
+        System.out.println("end Test case: testCase02");
+        }
+
+    public void testCase03() throws InterruptedException {
+        System.out.println("Start Test case: testCase03");
+        driver.findElement(By.xpath("(//a[@class ='h-5 hover:text-blue-s dark:hover:text-dark-blue-s'])[2]")).click();
+        Thread.sleep(5000);
+        String url = driver.getCurrentUrl();
+        if (url.contains("two-sum")) {
+            System.out.println("The url of the problem contains two-sum");
+        } else
+            System.out.println("The url of the problem doesn't contains two-sum");
+        System.out.println("end Test case: testCase03");
+        }
+
+    public void testCase04() throws InterruptedException {
+        System.out.println("Start Test case: testCase04");
+        driver.findElement(By.xpath("//div[@class='normal absolute left-0 top-0 whitespace-nowrap font-normal' and text()='Submissions']")).click();
+        Thread.sleep(5000);
+        //boolean result = driver.findElement(By.xpath("//a[text()='Register or Sign In']")).isDisplayed();
+        boolean result = driver.findElement(By.xpath("//span[contains(text(),'need')]")).isDisplayed();
+
+        if(result){
+            System.out.println("Register or Sign In is displayed");
+        }
+        else
+            System.out.println("Register or Sign In is Not displayed");
+        System.out.println("end Test case: testCase04");
+    }
 }
+
+
